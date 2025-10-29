fd27ab1d3bedbc929f645d13f1429ef276d11339
Kushal9753 <kushalsahu352@gmail.com>
2025-10-29 21:47:50 +0530

    Add addition scripts and a placeholder for square solution

---
+++ Simple/AddTowNum1.py
@@ -0,0 +1,4 @@
+num1 = int(input("enter first no :- "))
+num2 = int(input("enter second no :- "))
+
+print("addition of two number is:- ", num1+num2)
---
+++ Simple/AddTowNum2.py
@@ -0,0 +1,4 @@
+num1=10
+num2 = 20
+
+print("addition is :- ", num1 + num2)
