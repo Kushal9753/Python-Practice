ec1c1f53893a6f8c46df1e41757c4ddfa77ab0dc
Kushal9753 <kushalsahu352@gmail.com>
2025-10-30 23:13:36 +0530

    Add CheckVeOrZero.py to determine if a number is positive, negative, or zero

---
+++ Medium/Check+Ve-VeorZero.py
@@ -0,0 +1,9 @@
+''' Program to check if a number is positive, negative or zero '''
+num = int(input("Enter the number:- "))
+
+if num > 0:
+  print("Number is Positive")
+elif num < 0:
+  print("Number is Negative")
+else:
+  print("Number is Zero")
