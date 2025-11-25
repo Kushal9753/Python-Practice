d7d6a33090844cfa5427f3b0c42ce117c8ba9c6e
Kushal9753 <kushalsahu352@gmail.com>
2025-11-25 11:49:57 +0530

    Lambda

---
+++ Lambdafunction.py
@@ -0,0 +1,9 @@
+x = int(input("Enter first number :- "))
+y = int(input("Enter second number :- "))
+
+add = lambda a,b: a+b
+print(add(x,y))
+
+greet = lambda: "Hello!"
+print(greet())
+print(greet)
